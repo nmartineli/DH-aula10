39cb30234574625c6a1861a28e7aec60038d6820
nmartineli <nataliamartineli@gmail.com>
2021-03-03 21:50:36 -0300

    Finalizando hello world

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
+const listaUsuarios = [
+    'João',
+    'Maria',
+    'Carlos',
+    'Rafaela'
+]
 
-app.get('/', (req, res) => {
-    return res.send('Hello world!');
-})
+// Route Params
+//app.get('/:usuario', (req, res) => {
+//    const { usuario } = req.params;
+//    return res.send(`Hello world! ${usuario}`);
+//})
+
+// Query Params
+// http://localhost:3000/usuarios?nome=Maria
+
+app.get('/usuarios', (req, res) => {
+    const { nome } = req.query;
+    let listaRetorno = listaUsuarios.filter(i => i.includes(nome || ""));
+    return res.json(listaRetorno); 
+});
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
